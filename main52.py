373eb0ccd9a29211c4a1085d1cb0e49f2233f1be
Yurii1960 <jura.ntu.most@gmail.com>
2025-07-07 17:38:46 +0300

    HW 5.2

--- main52.py
+++ main52.py
@@ -1,25 +1,17 @@
 import re
 
-text= "dfdfdfd 333.33 fhfhfh []/.,n 90.09 "
 def generator_number(text):
     pattern=r'\s\d+\.\d+\s' 
     list_digits=re.findall(pattern,text)
-    
     for digit in list_digits:
-        
         yield float(digit)
-    #def sum_profit():
-        #sum=0
-gen=generator_number(text)        #sum=sum+
-print(next(gen))
-print(next(gen))
+        
+        
 def sum_profit(text,generator_number):
     sum=0
     print(list(generator_number(text)))
     for i in generator_number(text):
         sum=sum+i
-        print(i)
-    print(sum)
     return sum
-sum_profit(text,generator_number)
+
 
