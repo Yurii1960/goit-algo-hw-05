2387d4b2540ecc426de3b38cb96e8fd96debabb9
Yurii1960 <jura.ntu.most@gmail.com>
2025-07-15 22:27:04 +0300

    hw 5 -2 .2

--- main52.py
+++ main52.py
@@ -1,7 +1,7 @@
 import re
 
 def generator_number(text):
-    pattern=r'\d+\.\d+' 
+    pattern=r' \d+\.\d+ ' 
     list_digits=re.findall(pattern,text)
     for digit in list_digits:
         yield float(digit)
